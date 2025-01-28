159f33909f1c8faff0edc7c87e368aa8ebe8d827
MusicLovingWolf <randommusiclovingwolf@gmail.com>
2025-01-28 21:58:17 +0200

    AAA

--- src/app/laboratory-project/laboratory-project.component.ts
+++ src/app/laboratory-project/laboratory-project.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Language } from '../laboratory/laboratory.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-laboratory-project',
-  imports: [],
-  templateUrl: './laboratory-project.component.html',
-  styleUrl: './laboratory-project.component.css'
+  selector: 'app-project-card',
+  templateUrl: './project-card.component.html',
+  styleUrls: ['./project-card.component.css'],
+  imports: [CommonModule]
 })
 export class LaboratoryProjectComponent {
+  // get randomColors(): string[] {
+  //   return Array.from({ length: 2 }, () =>
+  //     '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6)
+  // );
+  // }
 
+  // language: Language | undefined = Language.Python;
+  // project_name: string = 'Hello!';
 }
--- src/app/laboratory/laboratory.component.ts
+++ src/app/laboratory/laboratory.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { LaboratoryProjectComponent } from '../laboratory-project/laboratory-project.component';
 
 @Component({
   selector: 'app-laboratory',
-  imports: [],
-  templateUrl: './laboratory.component.html',
+  imports: [LaboratoryProjectComponent],
+  template: `<app-project-card></app-project-card>`,
   styleUrl: './laboratory.component.css'
 })
 export class LaboratoryComponent {
 
+}
+
+export enum Language {
+  Python = 'Python',
+  Java = 'Java',
+  JavaScript = 'Javascript',
+  Ruby = 'Ruby',
+  Go = 'Go',
+  Cpp = 'C++',
+  PHP = 'PHP',
+  Swift = 'Swift',
+  Kotlin = 'Kotlin',
+  Csharp = 'C#'
 }
