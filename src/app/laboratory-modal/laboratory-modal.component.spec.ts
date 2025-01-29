a266618bed0c0734db62a8f7b37b8196fc90d682
Valkach <valentinminkac@gmail.com>
2025-01-29 14:43:11 +0200

    Added stuff on Loboratory page etc.

--- src/app/laboratory-modal/laboratory-modal.component.spec.ts
+++ src/app/laboratory-modal/laboratory-modal.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LaboratoryModalComponent } from './laboratory-modal.component';
 
 describe('LaboratoryModalComponent', () => {
--- src/app/laboratory/laboratory.component.spec.ts
+++ src/app/laboratory/laboratory.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LaboratoryComponent } from './laboratory.component';
 
 describe('LaboratoryComponent', () => {
