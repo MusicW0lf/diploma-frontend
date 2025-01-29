916608cdbc39a2146984861f21c95ce85197ac45
Valkach <valentinminkac@gmail.com>
2025-01-29 21:51:14 +0200

    Fixed LogIn page and stuff for SignUp page

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 import { RegistrationLoginComponent } from '../registration-login/registration-login.component';
+import { RegistrationSignupComponent } from '../registration-signup/registration-signup.component';
 
 @Component({
   selector: 'app-registration',
-  imports: [RegistrationLoginComponent],
+  imports: [RegistrationLoginComponent, RegistrationSignupComponent],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    this.location.back();
+  }
 }
