8f02cfe6fadd982c2c3f02c66e7320660d9b4adc
Valkach <valentinminkac@gmail.com>
2025-01-29 15:42:35 +0200

    Theme page

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AlgorithmFooterComponent } from '../algorithm-footer/algorithm-footer.component';
+import { AlgorithmHeaderComponent } from '../algorithm-header/algorithm-header.component';
 
 @Component({
   selector: 'app-lessons',
-  imports: [],
+  imports: [AlgorithmFooterComponent, AlgorithmHeaderComponent],
   templateUrl: './lessons.component.html',
   styleUrl: './lessons.component.css'
 })
