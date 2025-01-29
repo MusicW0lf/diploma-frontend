7006162dd675b08118baf05e1241adc7e4cfd5d0
Valkach <valentinminkac@gmail.com>
2025-01-29 16:28:19 +0200

    A lot of changes

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { AlgorithmFooterComponent } from '../algorithm-footer/algorithm-footer.component';
 import { AlgorithmHeaderComponent } from '../algorithm-header/algorithm-header.component';
+import { LessonsNavbarComponent } from '../lessons-navbar/lessons-navbar.component';
 
 @Component({
   selector: 'app-lessons',
-  imports: [AlgorithmFooterComponent, AlgorithmHeaderComponent],
+  imports: [AlgorithmFooterComponent, AlgorithmHeaderComponent,
+            LessonsNavbarComponent
+  ],
   templateUrl: './lessons.component.html',
   styleUrl: './lessons.component.css'
 })
--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { AlgorithmFooterComponent } from '../algorithm-footer/algorithm-footer.component';
+import { AlgorithmHeaderComponent } from '../algorithm-header/algorithm-header.component';
+import { SubscriptionFaqComponent } from '../subscription-faq/subscription-faq.component';
+import { SubscriptionOffersComponent } from '../subscription-offers/subscription-offers.component';
 
 @Component({
   selector: 'app-subscription',
-  imports: [],
+  imports: [AlgorithmFooterComponent, AlgorithmHeaderComponent,
+            SubscriptionFaqComponent, SubscriptionOffersComponent],
   templateUrl: './subscription.component.html',
   styleUrl: './subscription.component.css'
 })
 export class SubscriptionComponent {
+    faqItems = [
+      { question: "What’s the benefit and details of the subscription plans?", answer: "Subscription allows access to advanced courses, support, and features not available to free users." },
+      { question: "What happens if I cancel my subscription?", answer: "You will continue to enjoy all benefits until the end of your subscription term, after which access will be revoked." },
+      { question: "Can I subscribe multiple accounts at once?", answer: "Yes, you can subscribe to multiple accounts simultaneously, but each account needs its own subscription." },
+      { question: "How can I upgrade or downgrade my subscription?", answer: "You can upgrade or downgrade your subscription at any time through your account settings." }
+    ];
 
+    activeIndex: number | null = null;
+
+    toggleAnswer(index: number) {
+      this.activeIndex = this.activeIndex === index ? null : index;
+    }
 }
