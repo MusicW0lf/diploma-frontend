c7958914cbfe6e39e1eb53f64617cb3daed0475c
MusicLovingWolf <randommusiclovingwolf@gmail.com>
2025-01-29 15:34:24 +0200

    Fixed modal component

--- src/app/laboratory-language/laboratory-language.component.ts
+++ src/app/laboratory-language/laboratory-language.component.ts
@@ -1,33 +1,36 @@
 import { Component } from '@angular/core';
-import { Language } from '../laboratory/laboratory.component';
 import { CommonModule } from '@angular/common';
-import { LaboratoryModalComponent } from '../laboratory-modal/laboratory-modal.component';
+import { ModalService } from '../laboratory-modal/laboratory-modal-service';  // Import the modal service
+
+export enum Language {
+  Python = 'Python',
+  JavaScript = 'JavaScript',
+  Java = 'Java',
+  // Add more languages as necessary
+}
 
 @Component({
   selector: 'app-laboratory-language',
   standalone: true,
-  imports: [CommonModule, LaboratoryModalComponent],
+  imports: [CommonModule],
   templateUrl: './laboratory-language.component.html',
-  styleUrl: './laboratory-language.component.css'
+  styleUrls: ['./laboratory-language.component.css']
 })
 export class LaboratoryLanguageComponent {
-  isDropdownOpen = false;
-  isModalOpen = false;
   selectedLanguage: string = '';
-  languages = Object.values(Language);
+  isDropdownOpen: boolean = false;
+  languages = Object.values(Language);  // Using the Language enum values for dropdown
 
+  constructor(private modalService: ModalService) {}
+
+  // Open and close the dropdown
   setDropdownState(state: boolean) {
     this.isDropdownOpen = state;
   }
 
+  // When a language is selected, open the modal with the selected language
   selectLanguage(language: string) {
     this.selectedLanguage = language;
-    this.isModalOpen = true;
-    this.isDropdownOpen = false; // Close dropdown after selection
-  }
-
-  closeModal() {
-    this.isModalOpen = false;
-    this.selectedLanguage = ''; // Reset selected language
+    this.modalService.openModal(language);  // Pass language to ModalService to open the modal
   }
 }
