c5f9e30997db77a8f2ebf348ce86abdf88150506
MusicLovingWolf <randommusiclovingwolf@gmail.com>
2025-02-02 19:28:29 +0200

    Added registration

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
-import { Location } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { RegistrationLoginComponent } from '../registration-login/registration-login.component';
 import { RegistrationSignupComponent } from '../registration-signup/registration-signup.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
-  imports: [RegistrationLoginComponent, RegistrationSignupComponent],
+  imports: [RegistrationLoginComponent, RegistrationSignupComponent, CommonModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
-  constructor(private location: Location) {}
+  constructor(private location: Location, private route: ActivatedRoute) {}
 
   goBack(): void {
     this.location.back();
   }
+
+  mode: 'login' | 'signup' = 'login';
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe(params => {
+      const paramMode = params.get('mode');
+      if (paramMode === 'signup' || paramMode === 'login') {
+        this.mode = paramMode;
+      }
+    });
+  }
+  
 }
